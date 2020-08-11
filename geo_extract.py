36d21832828465542fe09dfb7046e691f52f9814
Everton Spader <everton.spader@gmail.com>
2020-08-11 17:10:52 +0200

    Begin scripting heic2jpg converter

--- geo_extract.py
+++ geo_extract.py
@@ -1,100 +1,56 @@
 import os
+import io
 from functools import partial
-from PIL import Image
-from PIL.ExifTags import TAGS, GPSTAGS
-
 from geopy.geocoders import Nominatim
+from ImageMetaData import ImageMetaData
 
 
-class ImageMetaData(object):
+def get_file_list(path):
+    """Get the list of files in a specifc path"""
+    files_list = [f for f in os.listdir(path) if (
+        os.path.isfile(os.path.join(path, f) and f != '.DS_Store'))]
+    return files_list
 
-    exif_data = None
-    image = None
 
-    def __init__(self, img_path):
-        self.image = Image.open(img_path)
-        self.get_exif_data()
-        super(ImageMetaData, self).__init__()
+def heic_to_jpeg(path):
+    """
+    Convert .HEIC files in a folder to the standard picture format
+    .jpg. This is needed because the iOS pictures standard when
+    transfered is in .HEIC format and it's not yet supported by
+    Python's 'pillow' library.
+    """
+    files_list = get_file_list(path)
+    count = 0
 
-    def get_exif_data(self):
-        """
-        Return a dictionary of the EXIF metadata from the pictures with tags
-        decoded to human readable information
-        """
-        exif_data = {}
-        info = self.image.getexif()
+    for img in files_list:
+        name, ext = os.path.basename(img).split('.')
 
-        if info:
-            for tag_id, value in info.items():
-                tag_decoded = TAGS.get(tag_id, tag_id)
-                if tag_decoded == 'GPSInfo':
-                    gps_data = {}
-                    for t in value:
-                        sub_decoded = GPSTAGS.get(t, t)
-                        gps_data[sub_decoded] = value[t]
+        if ext.lower() == 'heic':
+            s = io.BytesIO()
+            pi = Image.frombytes()
 
-                    exif_data[tag_decoded] = gps_data
-                else:
-                    exif_data[tag_decoded] = value
 
-        self.exif_data = exif_data
-        return exif_data
-
-    def get_if_exist(self, data, key):
-        """Return the value of a tag if it exists in the Exif data"""
-        if key in data:
-            return data[key]
-        return None
-
-    def gps_to_degrees(self, value):
-        """
-        Converts the GPS coordinates of the EXIF metadata into readable
-        degrees, minutes and seconds GPS coordinates
-        """
-        d = float(value[0])
-
-        m = float(value[1])
-
-        s = float(value[2])
-
-        return d + (m/60.0) + (s/3600.0)
-
-    def get_lat_lon(self):
-        """Returns the latitude and longitude if available in the exif data"""
-
-        lat = None
-        lon = None
-        exif_data = self.get_exif_data()
-
-        if 'GPSInfo' in exif_data:
-            gps_info = exif_data['GPSInfo']
-            gps_latitude = self.get_if_exist(gps_info, 'GPSLatitude')
-            gps_latitude_ref = self.get_if_exist(gps_info, 'GPSLatitudeRef')
-            gps_longitude = self.get_if_exist(gps_info, 'GPSLongitude')
-            gps_longitude_ref = self.get_if_exist(gps_info, 'GPSLongitudeRef')
-
-            if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
-                lat = self.gps_to_degrees(gps_latitude)
-                if gps_latitude_ref != 'N':
-                    lat = -lat
-
-                lon = self.gps_to_degrees(gps_longitude)
-                if gps_longitude_ref != 'E':
-                    lon = - lon
-
-        return lat, lon
+    if count:
+        print(f"{count} images successfully converted to .jpg")
+        return img_list
+    else:
+        return files_list
 
 
 path_name = os.path.join(os.getcwd(), 'pictures')
-print(path_name)
-img_path = os.path.join(path_name, os.listdir(path_name)[0])
-print(img_path)
-meta_data = ImageMetaData(img_path)
-lat, lon = meta_data.get_lat_lon()
-# data = meta_data.get_exif_data()['GPSInfo']
-print(lat, lon)
+img_list = heic_to_jpeg(path_name)
+img_location_list = []
 
-lat_lon = f"\"{lat}, {lon}\""
+for img in img_list:
+    img_path = os.path.join(path_name, img)
+    meta_data = ImageMetaData(img_path)
+    lat, lon = meta_data.get_lat_lon()
+
+    img_location_list.append({'filename': img,
+                              'file_path': img_path,
+                              'latitude': lat,
+                              'longitude': lon})
+
 
 geolocator = Nominatim(user_agent="geo_pic_finder")
 reverse = partial(geolocator.reverse, language="en")
