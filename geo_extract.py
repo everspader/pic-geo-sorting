8b60060133e0d93bc781db69b2db82a0e2cf5020
Everton Spader <everton.spader@gmail.com>
2020-09-02 17:27:48 -0300

    Added function for creating and sorting into folders

--- geo_extract.py
+++ geo_extract.py
@@ -1,5 +1,5 @@
 import os
-import io
+import shutil
 from functools import partial
 from geopy.geocoders import Nominatim
 from ImageMetaData import ImageMetaData
@@ -34,14 +34,30 @@ for img in jpg_img_list:
         lat, lon = meta_data.get_lat_lon()
         location = reverse([lat, lon])
         country = location.raw['address']['country']
+        jpg_img_data_list.append({'filename': img,
+                            'file_path': img_path,
+                            'metadata': meta_data,
+                            'latitude': lat,
+                            'longitude': lon,
+                            'country': country})
     except:
         pass
 
-    jpg_img_data_list.append({'filename': img,
-                          'file_path': img_path,
-                          'metadata': meta_data,
-                          'latitude': lat,
-                          'longitude': lon,
-                          'country': country})
+def create_folders_for_pictures(img_list):
+    """
+    Create a folder for each distinct country and move the specific
+    picture files into the respective folders.
+    """
+    path_name = os.path.join(os.getcwd(), 'pictures')
 
+    for img in img_list:
+        img_path = os.path.join(path_name, img['country'])
+        if not os.path.exists(img_path):
+            os.makedirs(img_path)
+
+        source = os.path.join(path_name, img['filename'])
+        dest = os.path.join(img_path, img['filename'])
+        shutil.move(source, dest)
+
+create_folders_for_pictures(jpg_img_data_list)
 print(jpg_img_data_list)
