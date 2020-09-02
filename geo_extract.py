fdc218320221b6df717a47ba521842b85e584788
Everton Spader <everton.spader@gmail.com>
2020-09-02 17:07:02 -0300

    Fixed for extracting data from jpg files

--- geo_extract.py
+++ geo_extract.py
@@ -3,10 +3,6 @@ import io
 from functools import partial
 from geopy.geocoders import Nominatim
 from ImageMetaData import ImageMetaData
-from PIL import Image
-import pyheif
-import filetype
-import exifread
 
 
 def get_file_list(path):
@@ -15,44 +11,37 @@ def get_file_list(path):
         os.path.isfile(os.path.join(path, f)) and f != '.DS_Store')]
     return files_list
 
-
-
-
-    if count:
-        print(f"{count} images successfully converted to .jpg")
-        return get_file_list(path)
-    else:
-        return files_list
+def jpg_img_list(img_list):
+    """Return a list of accepted jpg files from the pictures list"""
+    jpg_ext = ['.jpg', '.jpeg']
+    jpg_list = [img for img in img_list if (
+        os.path.splitext(img)[-1].lower() in jpg_ext)]
+    return jpg_list
 
 
 path_name = os.path.join(os.getcwd(), 'pictures')
-# img_list = heic_to_jpeg(path_name)
-img_list = get_file_list(path_name)
-# TO DO: Filter only JPG files
-img_data_list = []
-
+jpg_img_list = jpg_img_list(get_file_list(path_name))
+jpg_img_data_list = []
 geolocator = Nominatim(user_agent="geo_pic_finder")
 reverse = partial(geolocator.reverse, language="en")
 
-for img in img_list:
-    name, ext = os.path.splitext(img)
+for img in jpg_img_list:
+    # name, ext = os.path.splitext(img)
     img_path = os.path.join(path_name, img)
 
-    if ext.lower() != '.heic':
+    try:
         meta_data = ImageMetaData(img_path)
         lat, lon = meta_data.get_lat_lon()
         location = reverse([lat, lon])
-        breakpoint()
         country = location.raw['address']['country']
+    except:
+        pass
 
-    img_data_list.append({'filename': img,
-                          'filetype': ext,
+    jpg_img_data_list.append({'filename': img,
                           'file_path': img_path,
                           'metadata': meta_data,
                           'latitude': lat,
                           'longitude': lon,
                           'country': country})
 
-# location = reverse([lat, lon])
-# print(location.raw['address']['country'])
-print(img_location_list)
+print(jpg_img_data_list)
